c9510d143927afde5582915ec47d8ae496a19e96
hguzmant <hguzmant@miumg.edu.gt>
2022-10-27 22:53:35 -0600

    Comment

--- Quiniela/Controllers/EquipoController.cs
+++ Quiniela/Controllers/EquipoController.cs
@@ -42,7 +42,7 @@ namespace Quiniela.Controllers
         }
 
         // PUT: api/Equipo/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        
         [HttpPut("{id}")]
         public async Task<IActionResult> PutEquipo(int id, Equipo equipo)
         {
